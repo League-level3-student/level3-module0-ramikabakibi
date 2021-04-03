0f34f882127682a87f786c112a101d8b8c968746
aymankabakibi <aymankabakibi@imacs-imac.lan>
2021-04-03 11:27:13 -0700

    Finished everything in 03 intro to 2d arrays and 04 crazy digital painting. Need to start on the first one in 05 Pixel Art

--- src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
+++ src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
@@ -3,12 +3,21 @@ package _03_Intro_to_2D_Arrays;
 public class _02_RowColumnSum {
 
     public int getSumByRow(int[][] matrix, int row) {
-        
-        return 0;
+        int rowSum=0;
+   
+       for(int i=0; i<matrix[row].length; i++) {
+    	   rowSum+= matrix[row][i];
+       }
+     return rowSum;
     }
     
     public int getSumByColumn(int[][] matrix, int col) {
+        int colSum=0;
         
-        return 0;
+        for(int i=0; i<matrix.length; i++) {
+        	colSum+= matrix[i][col];
+        }
+        
+        return colSum;
     }
 }
